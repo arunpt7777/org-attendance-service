19fcd9ee6e4c7ee488aa07bec4d5f69a69361488
arunpt7777@gmail.com <arunpt7777@gmail.com>
2024-07-30 22:21:44 +0530

    US36 - Added Swagger to all services

--- src/main/java/com/motta/attendance_service/AttendanceServiceApplication.java
+++ src/main/java/com/motta/attendance_service/AttendanceServiceApplication.java
@@ -2,12 +2,25 @@ package com.motta.attendance_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AttendanceServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AttendanceServiceApplication.class, args);
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOriginPatterns("http://localhost:3000");
+			}
+		};
 	}
 
 }
